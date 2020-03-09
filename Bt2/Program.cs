142a9293710b30ae05231c7ddbf69803c76d2f01
Lynguyen9810@gmail.com <Lynguyen9810@gmail.com>
2020-03-09 08:50:33 +0700

    Add project files.

---
+++ Bt2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Bt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Begin timing.
+            stopwatch.Start();
+
+            // Do something.
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(5);
+            }
+
+            // Stop timing.
+            stopwatch.Stop();
+
+            // Write result.
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+}
